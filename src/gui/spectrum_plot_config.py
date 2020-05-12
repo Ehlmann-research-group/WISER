fab6dbb55d5c954fd9e7a8dca6d27f333ff3d9f3
Donnie Pinkston <donnie@cms.caltech.edu>
2020-05-12 13:51:03 -0700

    More spectrum context-menu functionality
    Most of the context menu operations in the spectrum-list tree widget are
    now functional.  This includes showing and hiding individual spectra,
    showing and hiding all spectra in a group, unloading spectral libraries,
    clearing the collected spectra, deleting individual spectra, and editing
    the active or collected spectrum information.
    
    The main feature that remains to be added is saving the collected
    spectra to a file.

--- src/gui/spectrum_plot_config.py
+++ src/gui/spectrum_plot_config.py
@@ -3,12 +3,11 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 from .spectrum_plot_config_ui import Ui_SpectrumPlotConfig
-from raster.spectra import SpectrumType, SpectrumAverageMode
+from raster.spectra import SpectrumAverageMode
 
 
 class SpectrumPlotConfigDialog(QDialog):
     def __init__(self, parent=None,
-                 default_click_spectrum_type=SpectrumType.SINGLE_PIXEL,
                  default_avg_mode=SpectrumAverageMode.MEAN,
                  default_area_avg_x=3, default_area_avg_y=3):
 
@@ -25,7 +24,6 @@ class SpectrumPlotConfigDialog(QDialog):
         if default_area_avg_y < 0 or default_area_avg_y % 2 != 1:
             raise ValueError(f'default_area_avg_y must be odd; got {default_area_avg_y}')
 
-        self._default_click_spectrum_type = default_click_spectrum_type
         self._default_avg_mode = SpectrumAverageMode.MEAN
         self._configure_comboboxes()
 
@@ -36,24 +34,12 @@ class SpectrumPlotConfigDialog(QDialog):
         self._ui.lineedit_area_avg_y.setText(str(default_area_avg_y))
 
     def _configure_comboboxes(self):
-        spectrum_types = {
-            SpectrumType.SINGLE_PIXEL : self.tr('Spectrum at pixel'),
-            SpectrumType.AREA_AVERAGE : self.tr('Area-average around pixel'),
-        }
-
         avg_modes = {
             SpectrumAverageMode.MEAN : self.tr('Mean'),
             SpectrumAverageMode.MEDIAN : self.tr('Median'),
         }
 
-        self._ui.combobox_spectrum_type.clear()
         self._ui.combobox_avg_mode.clear()
-
-        for (key, value) in spectrum_types.items():
-            self._ui.combobox_spectrum_type.addItem(value, key)
-
-        i = self._ui.combobox_spectrum_type.findData(self._default_click_spectrum_type)
-        self._ui.combobox_spectrum_type.setCurrentIndex(i)
 
         for (key, value) in avg_modes.items():
             self._ui.combobox_avg_mode.addItem(value, key)
@@ -70,13 +56,9 @@ class SpectrumPlotConfigDialog(QDialog):
                 self.tr('Area-average values must be odd.'), QMessageBox.Ok)
             return
 
-        self._default_click_spectrum_type = self._ui.combobox_spectrum_type.currentData()
         self._default_avg_mode = self._ui.combobox_avg_mode.currentData()
 
         super().accept()
-
-    def get_default_click_spectrum_type(self):
-        return self._default_click_spectrum_type
 
     def get_default_avg_mode(self):
         return self._default_avg_mode
