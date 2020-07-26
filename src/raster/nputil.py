ed4acd7c0b93b97a6604eafd1433bd8d9954925b
Donnie Pinkston <donnie@cms.caltech.edu>
2020-07-26 12:15:29 -0700

    Tests for raster.nputil module
    Also fixed a doc issue with the nputil.normalize() function.

--- src/raster/nputil.py
+++ src/raster/nputil.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def normalize(array: np.ndarray, minval=None, maxval=None) -> np.ndarray:
     '''
-    Normalize the specified array, either generating a new array, or updating
-    the passed-in array in place.  The minimum and maximum values can be
-    specified if already known, to allow the function to not have to compute
-    the minimum and maximum.
+    Normalize the specified array, generating a new array to return to the
+    caller.  The minimum and maximum values can be specified if already known,
+    or if the caller wants to normalize to a different min/max than the array's
+    actual min/max values.  NaN values are left unaffected.
     '''
 
     if minval is None:
---
+++ src/tests/test_raster_nputil.py
@@ -0,0 +1,60 @@
+import unittest
+
+import tests.context
+from raster.nputil import normalize
+
+import numpy as np
+
+
+class TestRasterNputil(unittest.TestCase):
+
+    #======================================================
+    # normalize()
+
+    def test_normalize_1d_no_minmax(self):
+        inp = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+
+        out = normalize(inp)
+
+        self.assertAlmostEqual(out[0], 0.00)
+        self.assertAlmostEqual(out[1], 0.25)
+        self.assertAlmostEqual(out[2], 0.50)
+        self.assertAlmostEqual(out[3], 0.75)
+        self.assertAlmostEqual(out[4], 1.00)
+
+    def test_normalize_1d_nans_no_minmax(self):
+        inp = np.array([np.nan, 1.0, 2.0, 3.0, np.nan, 4.0, 5.0])
+
+        out = normalize(inp)
+
+        self.assertTrue(np.isnan(out[0]))
+        self.assertAlmostEqual(out[1], 0.00)
+        self.assertAlmostEqual(out[2], 0.25)
+        self.assertAlmostEqual(out[3], 0.50)
+        self.assertTrue(np.isnan(out[4]))
+        self.assertAlmostEqual(out[5], 0.75)
+        self.assertAlmostEqual(out[6], 1.00)
+
+    def test_normalize_1d_minmax_specified(self):
+        inp = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+
+        out = normalize(inp, 2, 4)
+
+        self.assertAlmostEqual(out[0], -0.50)
+        self.assertAlmostEqual(out[1],  0.00)
+        self.assertAlmostEqual(out[2],  0.50)
+        self.assertAlmostEqual(out[3],  1.00)
+        self.assertAlmostEqual(out[4],  1.50)
+
+    def test_normalize_1d_nans_minmax_specified(self):
+        inp = np.array([np.nan, 1.0, 2.0, 3.0, np.nan, 4.0, 5.0])
+
+        out = normalize(inp, 2, 4)
+
+        self.assertTrue(np.isnan(out[0]))
+        self.assertAlmostEqual(out[1], -0.50)
+        self.assertAlmostEqual(out[2],  0.00)
+        self.assertAlmostEqual(out[3],  0.50)
+        self.assertTrue(np.isnan(out[4]))
+        self.assertAlmostEqual(out[5],  1.00)
+        self.assertAlmostEqual(out[6],  1.50)
