11e0a868397bf649aa93cd916179a16777ab8409
Donnie Pinkston <donnie@cms.caltech.edu>
2020-09-17 10:53:59 -0700

    A template for creating Secret.mk

---
+++ Secret-template.mk
@@ -0,0 +1,24 @@
+#=============================================================================
+# Template for creating a Secret.mk file
+#
+# Copy this file to Secret.mk, and edit Secret.mk with your secrets.
+#
+# Don't edit this file!
+#
+# Don't check in Secret.mk!
+
+#-----------------------------------------------------------------------------
+# macOS Secrets
+
+# This is the name of your code-signing key in your local keystore
+AD_CODESIGN_KEY_NAME="Your Key Name"
+
+# These are your Apple ID credentials for your developer account
+AD_USERNAME=your.appleid.email@example.com
+AD_PASSWORD=your-app-specific-password
+
+#-----------------------------------------------------------------------------
+# Windows Secrets
+
+# TODO
+
