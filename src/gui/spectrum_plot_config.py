1e828218e317dccffa5419fff35364370b7cd410
Donnie Pinkston <donnie@cms.caltech.edu>
2020-10-27 18:20:06 -0700

    Initial support for configuration of major/minor ticks
    The config dialog can now drive the inclusion/exclusion of major and
    minor ticks on the X and Y axis.  This support is still very buggy, due
    to the way that matplotlib specifies tick marks:  matplotlib expects a
    list of numeric values specifying ticks.  This means that when the plot
    changes the range being displayed in the X/Y axis, the tick positions
    need to be recalculated.  This will need to be supported in a future
    commit.  But, at least for now, the tick placement code seems to work.

--- src/gui/spectrum_plot_config.py
+++ src/gui/spectrum_plot_config.py
@@ -4,6 +4,44 @@ from PySide2.QtWidgets import *
 
 from .generated.spectrum_plot_config_ui import Ui_SpectrumPlotConfig
 from raster.spectra import SpectrumAverageMode
+from raster import units
+
+from typing import List
+
+
+def generate_ticks(min_value: float, max_value: float, tick_interval: float) -> List[float]:
+    '''
+    Given the specified range and tick interval, this function returns a list of
+    values within that range where tick marks are to appear.  If no tick marks
+    will fall within that range, an empty list is returned.  For example, if the
+    min_value is 5 and the max_value is 7, and the tick_interval is 100, an
+    empty list will be returned since no tick marks will fall within that range.
+    '''
+
+    # print(f'range=[{min_value} , {max_value}] - interval={tick_interval}')
+
+    ticks = []
+
+    # Instead of looping on specific tick values, which has the potential to
+    # accumulate errors, compute the integer "tick count," which is how many
+    # ticks to get past the min_value.  Then, we can increment the tick
+    # count to move forward, rather than adding in the tick_interval value.
+
+    tick_count = int(min_value // tick_interval)
+    if abs(tick_count * tick_interval - min_value) <= 1e-6:
+        # The start of the range is near a tick mark.
+        ticks.append(tick_count * tick_interval)
+
+    while True:
+        tick_count += 1
+        tick_value = tick_count * tick_interval
+        if tick_value > max_value:
+            break
+
+        ticks.append(tick_value)
+
+    # print(f'ticks={ticks}')
+    return ticks
 
 
 class SpectrumPlotConfigDialog(QDialog):
@@ -36,10 +74,35 @@ class SpectrumPlotConfigDialog(QDialog):
 
         self._ui.ledit_xaxis_label.setText(axes.get_xlabel())
 
+        for (s, unit) in units.KNOWN_SPECTRAL_UNITS.items():
+            self._ui.cbox_xaxis_units.addItem(self.tr(s), unit)
+
+        self._init_tick_ui(axes.get_xticks(minor=False),
+                           self._ui.ckbox_xaxis_major_ticks,
+                           self._ui.ledit_xaxis_major_ticks)
+
+        self._init_tick_ui(axes.get_xticks(minor=True),
+                           self._ui.ckbox_xaxis_minor_ticks,
+                           self._ui.ledit_xaxis_minor_ticks)
+
+        # TODO(donnie):  Need to ask the spectrum-plot widget whether it is
+        #     doing auto-limits or manually specified limits.
+
         #==============================
         # Y-Axis tab
 
         self._ui.ledit_yaxis_label.setText(axes.get_ylabel())
+
+        self._init_tick_ui(axes.get_yticks(minor=False),
+                           self._ui.ckbox_yaxis_major_ticks,
+                           self._ui.ledit_yaxis_major_ticks)
+
+        self._init_tick_ui(axes.get_yticks(minor=True),
+                           self._ui.ckbox_yaxis_minor_ticks,
+                           self._ui.ledit_yaxis_minor_ticks)
+
+        # TODO(donnie):  Need to ask the spectrum-plot widget whether it is
+        #     doing auto-limits or manually specified limits.
 
         #==============================
         # New Spectra tab
@@ -59,6 +122,13 @@ class SpectrumPlotConfigDialog(QDialog):
             index = 0
         self._ui.cbox_default_avg_mode.setCurrentIndex(index)
 
+
+    def _init_tick_ui(self, ticks, checkbox, lineedit):
+        checkbox.setChecked(len(ticks) > 1)
+        if len(ticks) > 1:
+            lineedit.setText(f'{ticks[1] - ticks[0]}')
+        else:
+            lineedit.clear()
 
     def accept(self):
 
@@ -99,10 +169,42 @@ class SpectrumPlotConfigDialog(QDialog):
 
         self._axes.set_xlabel(self._ui.ledit_xaxis_label.text().strip())
 
+        if self._ui.ckbox_xaxis_major_ticks.isChecked():
+            interval = float(self._ui.ledit_xaxis_major_ticks.text().strip())
+            (lower, upper) = self._axes.get_xbound()
+            ticks = generate_ticks(lower, upper, interval)
+            self._axes.set_xticks(ticks, minor=False)
+        else:
+            self._axes.set_xticks([], minor=False)
+
+        if self._ui.ckbox_xaxis_minor_ticks.isChecked():
+            interval = float(self._ui.ledit_xaxis_minor_ticks.text().strip())
+            (lower, upper) = self._axes.get_xbound()
+            ticks = generate_ticks(lower, upper, interval)
+            self._axes.set_xticks(ticks, minor=True)
+        else:
+            self._axes.set_xticks([], minor=True)
+
         #==============================
         # Y-Axis tab
 
         self._axes.set_ylabel(self._ui.ledit_yaxis_label.text().strip())
+
+        if self._ui.ckbox_yaxis_major_ticks.isChecked():
+            interval = float(self._ui.ledit_yaxis_major_ticks.text().strip())
+            (lower, upper) = self._axes.get_ybound()
+            ticks = generate_ticks(lower, upper, interval)
+            self._axes.set_yticks(ticks, minor=False)
+        else:
+            self._axes.set_yticks([], minor=False)
+
+        if self._ui.ckbox_yaxis_minor_ticks.isChecked():
+            interval = float(self._ui.ledit_yaxis_minor_ticks.text().strip())
+            (lower, upper) = self._axes.get_ybound()
+            ticks = generate_ticks(lower, upper, interval)
+            self._axes.set_yticks(ticks, minor=True)
+        else:
+            self._axes.set_yticks([], minor=True)
 
         #==============================
         # New Spectra tab
