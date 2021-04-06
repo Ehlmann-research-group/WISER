1d4ced650908a427dcb94d457d5da8144a7157ec
Donnie Pinkston <donnie@cms.caltech.edu>
2021-04-05 17:54:06 -0700

    Logic to manage plugin paths in config dialog
    The WISER config dialog now allows users to enter and remove plugin
    paths.  These paths are not yet used to update the WISER Python path;
    this will be a future update.  Also, this doesn't include the plugin
    list functionality yet.

--- src/gui/app_config_dialog.py
+++ src/gui/app_config_dialog.py
@@ -26,11 +26,16 @@ class AppConfigDialog(QDialog):
 
         self._app_state = app_state
 
+        self._init_general_tab()
+        self._init_plugins_tab()
+
+
+    def _init_general_tab(self):
         #==============================
         # Error-Reporting group-box
 
         self._ui.ckbox_online_bug_reporting.setChecked(
-            app_state.get_config('general.online_bug_reporting'))
+            self._app_state.get_config('general.online_bug_reporting'))
 
         #==============================
         # Visible-Light group-box
@@ -39,9 +44,9 @@ class AppConfigDialog(QDialog):
         self._ui.ledit_green_wavelength.setValidator(QIntValidator())
         self._ui.ledit_blue_wavelength.setValidator(QIntValidator())
 
-        red = app_state.get_config('general.red_wavelength_nm')
-        green = app_state.get_config('general.green_wavelength_nm')
-        blue = app_state.get_config('general.blue_wavelength_nm')
+        red = self._app_state.get_config('general.red_wavelength_nm')
+        green = self._app_state.get_config('general.green_wavelength_nm')
+        blue = self._app_state.get_config('general.blue_wavelength_nm')
 
         self._ui.ledit_red_wavelength.setText(f'{red}')
         self._ui.ledit_green_wavelength.setText(f'{green}')
@@ -50,19 +55,21 @@ class AppConfigDialog(QDialog):
         #==============================
         # Raster Display group-box
 
-        self._ui.ledit_viewport_highlight_color.setText(app_state.get_config('raster.viewport_highlight_color'))
+        self._ui.ledit_viewport_highlight_color.setText(
+            self._app_state.get_config('raster.viewport_highlight_color'))
 
         self._ui.cbox_pixel_cursor_type.addItem(self.tr('Crosshair'), 'SMALL_CROSS')
         self._ui.cbox_pixel_cursor_type.addItem(self.tr('Large crosshair'), 'LARGE_CROSS')
         self._ui.cbox_pixel_cursor_type.addItem(self.tr('Crosshair with box'), 'SMALL_CROSS_BOX')
 
-        self._ui.ledit_pixel_cursor_color.setText(app_state.get_config('raster.pixel_cursor_color'))
+        self._ui.ledit_pixel_cursor_color.setText(
+            self._app_state.get_config('raster.pixel_cursor_color'))
 
         self._ui.btn_viewport_highlight_color.clicked.connect(self._on_choose_viewport_highlight_color)
         self._ui.btn_pixel_cursor_color.clicked.connect(self._on_choose_pixel_cursor_color)
 
         # Fetch the cursor type as a string
-        cursor = app_state.get_config('raster.pixel_cursor_type')
+        cursor = self._app_state.get_config('raster.pixel_cursor_type')
         index = self._ui.cbox_pixel_cursor_type.findData(cursor)
         if index == -1:
             index = 0
@@ -74,19 +81,34 @@ class AppConfigDialog(QDialog):
         self._ui.ledit_aavg_x.setValidator(QIntValidator(1, 99))
         self._ui.ledit_aavg_y.setValidator(QIntValidator(1, 99))
 
-        self._ui.ledit_aavg_x.setText(str(app_state.get_config('spectra.default_area_avg_x')))
-        self._ui.ledit_aavg_y.setText(str(app_state.get_config('spectra.default_area_avg_y')))
+        self._ui.ledit_aavg_x.setText(
+            str(self._app_state.get_config('spectra.default_area_avg_x')))
+        self._ui.ledit_aavg_y.setText(
+            str(self._app_state.get_config('spectra.default_area_avg_y')))
 
         self._ui.cbox_default_avg_mode.addItem(self.tr('Mean'  ), 'MEAN')
         self._ui.cbox_default_avg_mode.addItem(self.tr('Median'), 'MEDIAN')
 
         # Fetch the mode as a string
-        mode = app_state.get_config('spectra.default_area_avg_mode')
+        mode = self._app_state.get_config('spectra.default_area_avg_mode')
         index = self._ui.cbox_default_avg_mode.findData(mode)
         if index == -1:
             index = 0
         self._ui.cbox_default_avg_mode.setCurrentIndex(index)
 
+
+    def _init_plugins_tab(self):
+        plugin_paths = self._app_state.get_config('plugin_paths')
+        for p in plugin_paths:
+            item = QListWidgetItem(p)
+            item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+            self._ui.list_plugin_paths.addItem(item)
+
+        self._ui.btn_del_plugin_path.setEnabled(False)
+
+        self._ui.list_plugin_paths.itemSelectionChanged.connect(self._on_plugin_path_selection_changed)
+        self._ui.btn_add_plugin_path.clicked.connect(self._on_add_plugin_path)
+        self._ui.btn_del_plugin_path.clicked.connect(self._on_del_plugin_path)
 
     def _on_choose_viewport_highlight_color(self, checked):
         initial_color = QColor(self._ui.ledit_viewport_highlight_color.text())
@@ -100,6 +122,66 @@ class AppConfigDialog(QDialog):
         color = QColorDialog.getColor(parent=self, initial=initial_color)
         if color.isValid():
             self._ui.ledit_pixel_cursor_color.setText(color.name())
+
+
+    def _on_plugin_path_selection_changed(self):
+        # Enable or disable the "Remove path" button based on whether something
+        # is actually selected.
+        self._ui.btn_del_plugin_path.setEnabled(
+            len(self._ui.list_plugin_paths.selectedItems()) > 0)
+
+    def _on_add_plugin_path(self, checked=False):
+        path = QFileDialog.getExistingDirectory(parent=self,
+            caption=self.tr('Choose plugin path'))
+
+        if path:
+            # Make sure the path isn't already in the list of paths.
+            for i in range(self._ui.list_plugin_paths.count()):
+                item = self._ui.list_plugin_paths.item(i)
+                if item.text() == path:
+                    QMessageBox.information(self,
+                        self.tr('Path already in plugin paths'),
+                        self.tr('Path "{0}" already in plugin-paths list').format(path))
+                    return
+
+            # Add the path to the list widget.
+            item = QListWidgetItem(path)
+            item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+            self._ui.list_plugin_paths.addItem(item)
+
+    def _on_del_plugin_path(self, checked=False):
+        # Find the path or paths to be removed
+
+        paths: List[Tuple[int, str]] = []
+        for i in range(self._ui.list_plugin_paths.count()):
+            item = self._ui.list_plugin_paths.item(i)
+            if item.isSelected():
+                paths.append( (i, item.text()) )
+
+        # Get user confirmation
+
+        if len(paths) == 1:
+            msg = self.tr('Remove this plugin path?') + f'\n\n{paths[0][1]}'
+        else:
+            msg = self.tr('Remove these plugin paths?') + \
+                '\n' + '\n'.join([p[1] for p in paths])
+
+        result = QMessageBox.question(self, self.tr('Remove plugin paths?'), msg)
+        if result != QMessageBox.Yes:
+            return  # User decided not to remove the path(s).
+
+        # Delete all affected plugin paths.  Delete in decreasing index order,
+        # so that indexes aren't shifted/invalidated by deleting lower-index
+        # entries.
+        indexes = [p[0] for p in paths]
+        indexes.sort(reverse=True)
+        for i in indexes:
+            item = self._ui.list_plugin_paths.item(i)
+            if item.isSelected():
+                self._ui.list_plugin_paths.takeItem(i)
+
+        # Now, no paths should be selected
+        self._ui.btn_del_plugin_path.setEnabled(False)
 
 
     def accept(self):
