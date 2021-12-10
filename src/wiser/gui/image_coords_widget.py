7d436243e375fc878b32531b291f64151b5cd084
Donnie Pinkston <donnie@cms.caltech.edu>
2021-12-09 16:28:13 -0800

    Basic functionality for displaying geographic coordinates
    The WISER status bar can now show the geographic coordinates of clicked
    pixels.  The UI can handle datasets with no coordinate reference system
    (CRS), or with just a geographic CRS, or a projected CRS (which also
    contains a geographic CRS).  If a CRS has "degree" as the units, the UI
    is able to switch between degrees/minutes/seconds and just degrees.
    
    The "go to coordinate" functionality is only beginning to be done, but
    the display work is pretty far along.
    
    Still a lot of refining to do, but it's a pretty good step forward.

---
+++ src/wiser/gui/image_coords_widget.py
@@ -0,0 +1,184 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from osgeo import gdal, osr
+
+from .generated.image_coords_widget_ui import Ui_ImageCoordsWidget
+
+from .geo_coords_config import CoordinateDisplayConfig
+from .geo_coords_dialog import GeoCoordsDialog
+
+from wiser.raster import RasterDataSet
+
+
+def to_deg_min_sec(value):
+    '''
+    Convert a floating-point degrees value to a tuple containing
+    [degrees, minutes, seconds].
+    '''
+    deg = int(value)
+
+    value = (value - int(value)) * 60
+    min = int(value)
+
+    value = (value - int(value)) * 60
+    sec = value
+
+    return (deg, min, sec)
+
+def to_deg_min_sec_str(value, axis):
+    '''
+    Convert a floating-point degrees value to a string
+    '{degs}°{mins}\'{secs}"'.
+    '''
+    dms = to_deg_min_sec(value)
+    return f'{dms[0]}\N{DEGREE SIGN}{dms[1]}\'{dms[2]:.2f}"{axis}'
+
+
+class ImageCoordsWidget(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        # Set up the UI state
+        self._ui = Ui_ImageCoordsWidget()
+        self._ui.setupUi(self)
+
+        self._ui.tbtn_geo_goto.setIcon(QIcon(':/icons/geo-coords.svg'))
+
+        self._deg_min_sec = False
+
+        # The current dataset and pixel-coordinates being shown in the widget.
+        # We record these because when the config changes, we need to update
+        # the displayed information.
+        self._dataset: Optional[RasterDataSet] = None
+        self._pixel_coords = None
+
+        self._display_config: Dict[int, CoordinateDisplayConfig] = {}
+
+        self._ui.tbtn_geo_goto.clicked.connect(self._on_geo_dialog)
+
+
+
+    def _set_geo_visible(self, visible):
+        for w in [self._ui.lbl_geo, self._ui.lbl_geo_coords, self._ui.tbtn_geo_goto]:
+            w.setVisible(visible)
+
+    def _set_pixel_coords_text(self, pixel_coords):
+        self._ui.lbl_pixel_coords.setText(f'({pixel_coords[0]}, {pixel_coords[1]})')
+
+    def _set_geo_coords_text(self, pixel_coords, dataset, config):
+        self._ui.lbl_geo_coords.clear()
+        if config.spatial_ref is None:
+            return
+
+        xform = dataset.get_geo_transform()
+        ds_spatial_ref = dataset.get_spatial_ref()
+
+        print(f'Geo Transform = {xform}')
+        geo_coords = gdal.ApplyGeoTransform(xform, pixel_coords[0], pixel_coords[1])
+        print(f'Geo Coords = {geo_coords}')
+
+        if not config.spatial_ref.IsSame(ds_spatial_ref):
+            xform2 = osr.CoordinateTransformation(ds_spatial_ref, config.spatial_ref)
+            print(f'Geo Transform 2 = {xform2}')
+            geo_coords = xform2.TransformPoint(geo_coords[0], geo_coords[1])
+            print(f'Geo Coords 2 = {geo_coords}')
+
+        if config.spatial_ref.IsGeographic():
+            # Latitude/longitude.  Longitude comes out first, so flip 'em.
+
+            if config.spatial_ref.GetAngularUnitsName().lower().startswith('degree'):
+                ns_axis = 'N'
+                ew_axis = 'E'
+
+                if not config.use_negative_degrees:
+                    if geo_coords[1] < 0:
+                        geo_coords[1] = -geo_coords[1]
+                        ns_axis = 'S'
+
+                    if geo_coords[0] < 0:
+                        geo_coords[0] = -geo_coords[0]
+                        ew_axis = 'W'
+
+                if config.use_deg_min_sec:
+                    v1 = to_deg_min_sec_str(geo_coords[1], ns_axis)
+                    v2 = to_deg_min_sec_str(geo_coords[0], ew_axis)
+
+                else:
+                    v1 = f'{geo_coords[1]}\N{DEGREE SIGN}{ns_axis}'
+                    v2 = f'{geo_coords[0]}\N{DEGREE SIGN}{ew_axis}'
+
+            else:
+                v1 = f'{geo_coords[1]}'
+                v2 = f'{geo_coords[0]}'
+
+        else:
+            # Northing/Easting.
+            v1 = f'{geo_coords[0]}'
+            v2 = f'{geo_coords[1]}'
+
+        self._ui.lbl_geo_coords.setText(f'({v1}, {v2})')
+
+
+    def _get_config_for_dataset(self, dataset) -> CoordinateDisplayConfig:
+        ds_id = dataset.get_id()
+        if ds_id not in self._display_config:
+            # Generate a new configuration for the dataset.
+            spatial_ref = dataset.get_spatial_ref()
+
+            # We prefer the geographic CRS over the projected CRS
+            if spatial_ref is not None and spatial_ref.IsProjected():
+                spatial_ref = spatial_ref.CloneGeogCS()
+
+            self._display_config[ds_id] = CoordinateDisplayConfig(spatial_ref)
+
+        return self._display_config[ds_id]
+
+
+    def update_coords(self, dataset, pixel_coords):
+        self._dataset = dataset
+        self._pixel_coords = pixel_coords
+        self._update_internal()
+
+
+    def _update_internal(self):
+        if self._dataset is None or self._pixel_coords is None:
+            self.setVisible(False)
+            return
+
+        self.setVisible(True)
+
+        config = self._get_config_for_dataset(self._dataset)
+
+        if config.spatial_ref is None:
+            self._set_pixel_coords_text(self._pixel_coords)
+            self._set_geo_visible(False)
+
+        else:
+            self._set_pixel_coords_text(self._pixel_coords)
+            self._set_geo_coords_text(self._pixel_coords, self._dataset, config)
+            self._set_geo_visible(True)
+
+
+    def _on_geo_dialog(self):
+        if self._dataset is None:
+            return
+
+        config = self._get_config_for_dataset(self._dataset)
+        dialog = GeoCoordsDialog(self._dataset, config, parent=self)
+        dialog.config_changed.connect(self._on_display_config_changed)
+
+        # corner = self.mapToGlobal(QPoint(self.width(), 0.0))
+        # corner = QPoint(corner.x() - dialog.width(), corner.y() - dialog.height())
+        # dialog.move(corner)
+
+        dialog.exec()
+
+
+    def _on_display_config_changed(self, ds_id, config):
+        print(f'Received display-config changed notification:  {ds_id}, {config}')
+        self._display_config[ds_id] = config
+        self._update_internal()
