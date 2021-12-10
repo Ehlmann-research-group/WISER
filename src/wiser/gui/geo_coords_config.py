34ab09ccd400aaeba87f929e3ada127724d8219e
Donnie Pinkston <donnie@cms.caltech.edu>
2021-12-10 10:27:01 -0800

    Goto-coordinate transformation code is mostly complete
    A few other tweaks as well, but the main content of this commit is that
    the Geographic Coordinates dialog is now able to transform geographic
    coordinates in some CRS back into pixel coordinates.  There is still a
    lot of debug output at this point, but since it works and it was a bit
    tricky, it seemed good to get it checked in.
    
    Also improved the UI behavior a bit, disabling and enabling widgets
    based on whether the CRS was geographic or not.
    
    Also did a bit of code refactoring.

--- src/wiser/gui/geo_coords_config.py
+++ src/wiser/gui/geo_coords_config.py
@@ -15,3 +15,19 @@ class CoordinateDisplayConfig:
 
     def __str__(self):
         return f'CoordinateDisplayConfig[{self.spatial_ref}]'
+
+
+def srs_has_degrees(spatial_ref) -> bool:
+    return (spatial_ref is not None and
+            spatial_ref.IsGeographic() and
+            spatial_ref.GetAngularUnitsName().lower().startswith('degree'))
+
+
+def get_srs_units(spatial_ref) -> Optional[str]:
+    unit_name = None
+    if spatial_ref is not None:
+        if spatial_ref.IsProjected():
+            unit_name = spatial_ref.GetLinearUnitsName()
+        elif spatial_ref.IsGeographic():
+            unit_name = spatial_ref.GetAngularUnitsName()
+    return unit_name
