d1b336c5a31e1dcde486614b42c001d4b61e6eb6
JoshuaG-K <jgarciakimble@gmail.com>
2024-09-10 14:14:28 -0700

    Fixed ROI recalculating on visualization changes, haven't ran tests

--- src/wiser/gui/spectrum_info_editor.py
+++ src/wiser/gui/spectrum_info_editor.py
@@ -34,10 +34,22 @@ class SpectrumInfoEditor(QDialog):
         self._ui.combobox_avg_mode.activated.connect(self._on_aavg_mode_changed)
         self._ui.lineedit_area_avg_x.editingFinished.connect(self._on_finish_aavg)
         self._ui.lineedit_area_avg_y.editingFinished.connect(self._on_finish_aavg)
+    
+        self._initial_area_avg_x = None
+        self._initial_area_avg_y = None
+        self._initial_avg_mode = None
+        self._compute_values_changed = {
+            '_initial_area_avg_x' : False,
+            '_initial_area_avg_y' : False,
+            '_initial_avg_mode' : False
+        }
+
+        self.should_recalculate = False
 
 
     def configure_ui(self, spectrum: Spectrum):
         self._spectrum = spectrum
+        self._should_plot_update = False
 
         #========================================
         # Constants:
@@ -93,6 +105,7 @@ class SpectrumInfoEditor(QDialog):
             avg_mode = self._spectrum.get_avg_mode()
             i = self._ui.combobox_avg_mode.findData(avg_mode)
             self._ui.combobox_avg_mode.setCurrentIndex(i)
+            self._initial_avg_mode = i
 
             self._ui.combobox_avg_mode.setEnabled(True)
 
@@ -107,6 +120,8 @@ class SpectrumInfoEditor(QDialog):
 
             self._ui.lineedit_area_avg_x.setText(str(area_avg_x))
             self._ui.lineedit_area_avg_y.setText(str(area_avg_y))
+            self._initial_area_avg_y = str(area_avg_y)
+            self._initial_area_avg_x = str(area_avg_x)
 
             # Enable the area-average line edit widgets.
             for le in [self._ui.lineedit_area_avg_x, self._ui.lineedit_area_avg_y]:
@@ -125,6 +140,7 @@ class SpectrumInfoEditor(QDialog):
     def _on_choose_color(self, checked):
         initial_color = QColor(self._ui.lineedit_plot_color.text())
         color = QColorDialog.getColor(parent=self, initial=initial_color)
+        print("COLOR PRESSED")
         if color.isValid():
             # self._spectrum.set_color(color.name())
             self._ui.lineedit_plot_color.setText(color.name())
@@ -134,6 +150,7 @@ class SpectrumInfoEditor(QDialog):
         '''
         If the user edits the spectrum name, clear the "auto-generate name" flag
         '''
+        print("NAME EDITED")
         self._ui.ckbox_autogen_name.setChecked(False)
 
 
@@ -145,9 +162,30 @@ class SpectrumInfoEditor(QDialog):
         self._maybe_regenerate_name()
 
     def _on_aavg_mode_changed(self, index):
+        print("Average mode changed!!!")
+        print("self._ui.combobox_avg_mode.currentIndex(): ", self._ui.combobox_avg_mode.currentIndex())
+        print("self._initial_avg_mode: ", self._initial_avg_mode)
+        if self._ui.combobox_avg_mode.currentIndex() != self._initial_avg_mode:
+            self._compute_values_changed['_initial_avg_mode'] = True
+        else:
+            self._compute_values_changed['_initial_avg_mode'] = False
+            
         self._maybe_regenerate_name()
 
     def _on_finish_aavg(self):
+        print("Editing finished!!!")
+        print("self._ui.lineedit_area_avg_x.text() : ",  self._ui.lineedit_area_avg_x.text())
+        print("self._initial_area_avg_x : ", self._initial_area_avg_x)
+        if (self._ui.lineedit_area_avg_x.text() != self._initial_area_avg_x):
+            self._compute_values_changed['_initial_area_avg_x'] = True
+        else:
+            self._compute_values_changed['_initial_area_avg_x'] = False
+
+        if (self._ui.lineedit_area_avg_y.text() != self._initial_area_avg_y):
+            self._compute_values_changed['_initial_area_avg_y'] = True
+        else:
+            self._compute_values_changed['_initial_area_avg_y'] = False
+
         self._maybe_regenerate_name()
 
 
@@ -156,7 +194,6 @@ class SpectrumInfoEditor(QDialog):
         # Verify UI values before making any changes.
 
         # Name
-
         use_autogen_name = False  # RasterDataSetSpectrum-specific field
         if isinstance(self._spectrum, RasterDataSetSpectrum):
             use_autogen_name = self._ui.ckbox_autogen_name.isChecked()
@@ -215,6 +252,13 @@ class SpectrumInfoEditor(QDialog):
 
         self._spectrum.set_color(color_name)
 
+        
+        #=======================================================================
+        # Process values so object using this knows what changed
+
+        self.should_recalculate = any(self._compute_values_changed.values())
+        print("In Accept")
+        print("self.should_recalculate: ", self.should_recalculate)
         #=======================================================================
         # All done!
 
