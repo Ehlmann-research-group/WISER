4b191277cedb9a14784b9173004dbde99d0e1891
JoshuaG-K <jgarciakimble@gmail.com>
2024-09-27 15:30:33 -0700

    Refactored the multiple bands in band math

--- src/wiser/profiling/bandmath_benchmark.py
+++ src/wiser/profiling/bandmath_benchmark.py
@@ -100,11 +100,11 @@ results_new_method = {
 
 def benchmark_all_bandmath(hdr_paths: str, use_both_methods = False, use_old_method = False):
     equation_dict = {
-        # "+": '(a+b)+(c+d)',
-        "*": '(a*b)*(c*d)',
-        "/": '(a/b)/(c/d)',
-        "-": '(a-b)-(c-d)',
-        "<": '((a-b)-d)<c'
+        "+": '(a+b)+(c+d)',
+        # "*": '(a*b)*(c*d)',
+        # "/": '(a/b)/(c/d)',
+        # "-": '(a-b)-(c-d)',
+        # "<": '((a-b)-d)<c'
     }
 
     oper_file_time_dict = {}
