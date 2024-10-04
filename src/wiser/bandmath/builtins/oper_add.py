Testing why changing band size causes incorrect results

src/wiser/bandmath/builtins/oper_add.py
@@ -133,8 +133,8 @@ class OperatorAdd(BandMathFunction):
         (lhs, rhs) = reorder_args(lhs.type, rhs.type, lhs, rhs)
 
         async def async_read_gdal_data_onto_queue(index_list: List[int]):
-            if isinstance(lhs.value, np.ndarray):
-                print(f"RUH ROH RAGGY: \n lhs.value is {type(lhs.value)} with shape {lhs.value.shape}")
+            # if isinstance(lhs.value, np.ndarray):
+            #     print(f"RUH ROH RAGGY: \n lhs.value is {type(lhs.value)} with shape {lhs.value.shape}")
             future = event_loop.run_in_executor(read_thread_pool, lhs.as_numpy_array_by_bands, index_list)
             read_task_queue[LHS_KEY].put(future)
 
@@ -150,12 +150,12 @@ class OperatorAdd(BandMathFunction):
             band_index_list_sorted is sorted in increasing order i.e. [1, 3, 4, 8]'''
             total_num_bands, _, _ = lhs.get_shape()
             if lhs.is_intermediate:
-                print(f"LHS IS AN INTERMEDIATE VALUEEEEEEEEEEEEEE")
+                # print(f"LHS IS AN INTERMEDIATE VALUEEEEEEEEEEEEEE")
                 return False
-            else:
-                print("LHS IS NOT AN INTERMEDIATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+            # else:
+                # print("LHS IS NOT AN INTERMEDIATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
             if band_index_list_sorted == [] or band_index_list_sorted is None:
-                print("Was false")
+                # print("Was false")
                 return False
             # max_curr_band = band_index_list_sorted[-1]
             # print(f"result {max_curr_band} < {total_num_bands}: {max_curr_band < total_num_bands}")
@@ -167,26 +167,26 @@ class OperatorAdd(BandMathFunction):
             lhs_future = None
             if index_list_current is not None:
                 if lhs.type == rhs.type:
-                    print(f"TYPES ARE EQUAL< ASSERTING: {lhs.get_shape()}, {rhs.get_shape()}")
+                    # print(f"TYPES ARE EQUAL< ASSERTING: {lhs.get_shape()}, {rhs.get_shape()}")
                     assert(lhs.get_shape() == rhs.get_shape())
                 if isinstance(index_list_current, int):
                     index_list_current = [index_list_current]
                 # Check to see if queue is empty. 
                 if read_task_queue[LHS_KEY].empty():
-                    print(f"READING IO FUTURES QUEUE FOR NODE {node_id} IS EMPTY")
+                    # print(f"READING IO FUTURES QUEUE FOR NODE {node_id} IS EMPTY")
                     await async_read_gdal_data_onto_queue(index_list_current)
                     lhs_future = read_task_queue[LHS_KEY].get()
                 # If queue is not empty we pop from it
                 else:
-                    print (f"READING IO FUTURES QUEUE FOR NODE {node_id} IS NOT EMPTY")
+                    # print (f"READING IO FUTURES QUEUE FOR NODE {node_id} IS NOT EMPTY")
                     lhs_future = read_task_queue[LHS_KEY].get()
                 should_read_next = should_continue_reading_bands(index_list_next, lhs)
                 if should_read_next:
                     asyncio.create_task(async_read_gdal_data_onto_queue(index_list_next))
-                print(f"About to await for data for node {node_id}")
+                # print(f"About to await for data for node {node_id}")
                 # print(f"LHS FUTURE TYPE: {type(lhs_future)}")
                 lhs_value = await lhs_future # await asyncio.wrap_future(lhs_future)
-                print(f"Got data for node {node_id}")
+                # print(f"Got data for node {node_id}")
 
                 # Once the read task is done, we will just process the data and return like normal
                 # The processing does not take long enough to warrant creating a ProcessPoolExecutor
@@ -211,7 +211,11 @@ class OperatorAdd(BandMathFunction):
                 rhs_value = await rhs_future
                 # rhs_value = make_image_cube_compatible_by_bands(rhs, lhs_value.shape, index_list_current)
                 # print(f"we awaited rhs value and got: {type(rhs_value)}")
+                print("---------------------------RESULTS---------------------------")
+                print(f"lhs_value: {lhs_value[:,100:110,100:110]}, lhs is intermediate? {lhs.is_intermediate}")
+                print(f"rhs_value: {rhs_value[:,100:110,100:110]}, rhs is intermediate? {rhs.is_intermediate}")
                 result_arr = lhs_value + rhs_value
+                print(f"results_arr: {result_arr[:,100:110,100:110]}")
 
                 # The dimension should be two because we are slicing by band
                 assert result_arr.ndim == 3 or (result_arr.ndim == 2 and len(index_list_current) == 1)
@@ -224,7 +228,11 @@ class OperatorAdd(BandMathFunction):
                 assert lhs_value.ndim == 3
 
                 rhs_value = make_image_cube_compatible(rhs, lhs_value.shape)
+                print("---------------------------RESULTS OLD METHOD---------------------------")
+                print(f"lhs_value: {lhs_value[:,100:110,100:110]}, lhs is intermediate? {lhs.is_intermediate}")
+                print(f"rhs_value: {rhs_value[:,100:110,100:110]}, rhs is intermediate? {rhs.is_intermediate}")
                 result_arr = lhs_value + rhs_value
+                print(f"results_arr: {result_arr[:,100:110,100:110]}")
 
                 # The result array should have the same dimensions as the LHS input
                 # array.

src/wiser/bandmath/evaluator.py
@@ -1046,9 +1046,9 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
     # print('========')
     print_tree_with_meta(tree)
 
-    # numInterFinder = NumberOfIntermediatesFinder(lower_variables, lower_functions, expr_info.shape)
-    # number_of_intermediates = numInterFinder.transform(tree)
-    # print(f"Number of intermediates: {number_of_intermediates}")
+    numInterFinder = NumberOfIntermediatesFinder(lower_variables, lower_functions, expr_info.shape)
+    number_of_intermediates = numInterFinder.transform(tree)
+    print(f"Number of intermediates: {number_of_intermediates}")
     # print(f"TREE: \n {tree}")
     # print_tree_with_positions(tree)
     # print_tree_with_meta(tree)
@@ -1065,6 +1065,7 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
         return True
 
     def write_raster(out_dataset_gdal, band_index_list_current, result):
+        print("[[[[[[[[[[[[[[[[[[[AOBUT TO WRITE]]]]]]]]]]]]]]]]]]]")
         gdal_band_list_current = [band+1 for band in band_index_list_current]
         # Write queue data to be written all at once
         out_dataset_gdal.WriteRaster(
@@ -1075,6 +1076,8 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
             band_list=gdal_band_list_current
         )
         out_dataset_gdal.FlushCache()
+        print("(((((((((((((((((((((FINISHED FLUSHING)))))))))))))))))))))")
+
     from memory_profiler import memory_usage
     if expr_info.result_type == VariableType.IMAGE_CUBE and not use_old_method:
         eval = None
@@ -1091,7 +1094,7 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
             folder_path = os.path.dirname(result_path)
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
-            out_dataset_gdal = gdal.GetDriverByName('ENVI').Create(result_path, samples, lines, bands, gdal.GDT_CFloat32)
+            out_dataset_gdal = gdal.GetDriverByName('ENVI').Create(result_path, samples, lines, bands, gdal.GDT_Float32)
             # out_dataset_gdal.FlushCache()
             # out_dataset_gdal = None
 
@@ -1104,7 +1107,7 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
             #     print("Failed to reopen dataset with GDAL_OF_THREADSAFE flag.")
             bytes_per_scalar = SCALAR_BYTES
             max_bytes = MAX_RAM_BYTES/bytes_per_scalar
-            max_bytes_per_intermediate = max_bytes * 1 #number_of_intermediates
+            max_bytes_per_intermediate = max_bytes / 2 # number_of_intermediates
             num_bands = int(np.floor(max_bytes_per_intermediate / (lines*samples)))
             writing_futures = []
             memory_before = memory_usage()[0]
@@ -1135,6 +1138,8 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
                 print(f';;;;;;;;;;;;;;; type of result_value after: {type(result_value)}')
                 # result_value = result_value
                 res = result_value.value
+                # print("---------------------------EVAL RESULTS---------------------------")
+                # print(f"eval res: \n {res}")
                 if isinstance(result_value.value, np.ma.MaskedArray):
                     if not np.issubdtype(res.dtype, np.floating):
                         res = res.astype(np.float32)
@@ -1161,10 +1166,18 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
                 # )
                 # out_dataset_gdal.FlushCache()
 
+                # future = eval._event_loop.run_in_executor(eval._write_thread_pool, write_raster, \
+                #                                     out_dataset_gdal, band_index_list_current, res)
                 future = eval._write_thread_pool.submit(write_raster, \
                                                     out_dataset_gdal, band_index_list_current, \
-                                                    band_index_list_current, res)
+                                                    res)
                 writing_futures.append(future)
+            concurrent.futures.wait(writing_futures)
+            # for future in writing_futures:
+            #     print("waiting for future")
+            #     waiting_result = future.result()
+            # concurrent.futures.wait(writing_futures)
+            # eval._event_loop.run_until_complete(asyncio.gather(*writing_futures))
         except BaseException as e:
             print("SOME EXCEPTION !!!!!!!!!!!!")
             print
@@ -1175,6 +1188,8 @@ def eval_bandmath_expr(bandmath_expr: str, expr_info: BandMathExprInfo, result_n
             print(f"==========NEW MAX MEMORY USED: THROUGHOUT PROCESS {max_memory_used} MB============")
         concurrent.futures.wait(writing_futures)
         print(f"DONE WRITING FUTURES")
+        print(f"---------------------------Out dataset data:---------------------------")
+        print(f"{out_dataset_gdal.ReadAsArray()}")
             # Loop through band index list and add a write task to the executor
             # for gdal_band_index in band_index_list_current:
             #     future = eval._write_thread_pool.submit(write_band, \