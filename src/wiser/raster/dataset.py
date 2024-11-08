79427468c98fac50d69620d2773a3795c87186de
JoshuaG-K <jgarciakimble@gmail.com>
2024-11-07 16:48:10 -0800

    Testing still

--- src/wiser/raster/dataset.py
+++ src/wiser/raster/dataset.py
@@ -421,6 +421,7 @@ class RasterDataSet:
         the metadata will be set to NaN, and bands with the "data ignore value"
         will also be set to NaN.
         '''
+        print(f"dataset get_all_bands_at")
         arr = self._impl.get_all_bands_at(x, y)
 
         if filter_bad_values:
