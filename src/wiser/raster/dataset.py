Trying to fix average roi spectrum

src/wiser/raster/dataset.py
@@ -581,15 +581,25 @@ class RasterDataSet:
                         set_band(arr, i, np.nan)
                     except:
                         set_band(arr, i, DEFAULT_MASK_VALUE)
+                elif self._data_ignore_value is not None:
+                    data_ignore_mask = np.isclose(arr, self._data_ignore_value)
+                    if data_ignore_mask.any():
+                        # Band has the "data ignore" value
+                        try:
+                            arr[data_ignore_mask] = np.nan
+                        except BaseException as e:
+                            print(f"Exception occured when trying to mask array with np.nan:\n{e}")
+                            arr[data_ignore_mask] = DEFAULT_MASK_VALUE
 
-                elif (self._data_ignore_value is not None and
-                      math.isclose(arr[i], self._data_ignore_value)):
-                    # Band has the "data ignore" value
-                    try:
-                        set_band(arr, i, np.nan)
-                    except BaseException as e:
-                        print(f"Exception occured when trying to mask array with np.nan:\n{e}")
-                        set_band(arr, i, DEFAULT_MASK_VALUE)
+                    # for elem in np.nditer(arr):
+                    #     if (self._data_ignore_value is not None and
+                    #         math.isclose(arr[i], self._data_ignore_value)):
+                    #         # Band has the "data ignore" value
+                    #         try:
+                    #             set_band(arr, i, np.nan)
+                    #         except BaseException as e:
+                    #             print(f"Exception occured when trying to mask array with np.nan:\n{e}")
+                    #             set_band(arr, i, DEFAULT_MASK_VALUE)
         return arr
 
     def get_geo_transform(self) -> Tuple:

src/wiser/raster/spectrum.py
@@ -188,13 +188,22 @@ def calc_spectrum_fast(dataset: RasterDataSet, roi: RegionOfInterest,
     qrects = array_to_qrects(rects)
     for qrect in qrects:
         s = dataset.get_all_bands_at_rect(qrect.left(), qrect.top(), qrect.width(), qrect.height())
-        for spectrum in np.nditer(s):
-            spectra.append(spectrum)
+        ndim = s.ndim
+        if ndim == 2:
+            for i in range(s.shape[1]):
+                spectra.append(s[:,i])
+        elif ndim == 3:
+            for i in range(s.shape[1]):
+                for j in range(s.shape[2]):
+                    spectra.append(s[:,i,j])
+        else:
+            raise TypeError(f'Expected 2 or 3 dimensions in rectangular aray, but got {s.ndim}')
 
-    assert(len(spectra) == len(roi.get_all_pixels()))
+    assert(len(spectra) == len(roi.get_all_pixels()), f'Length of spectra is: {len(spectra)} while length of roi all pixels is: {len(roi.get_all_pixels())}')
 
     if len(spectra) > 1:
-        print("Spectra computing starting")
+        print(f"Mean Spectra computing starting: {len(spectra)}")
+        spectra = np.asarray(spectra)
         # Need to compute mean/median/... of the collection of spectra
         if mode == SpectrumAverageMode.MEAN:
             print("Spectra: ", type(spectra))