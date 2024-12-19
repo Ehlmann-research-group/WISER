1d10c076963476c9f37d0edde473ad9e08c517d0
JoshuaG-K <jgarciakimble@gmail.com>
2024-12-19 08:41:32 -0800

    Added tests for same data between panes

--- src/tests/test_roi_avg_spectrum.py
+++ src/tests/test_roi_avg_spectrum.py
@@ -1,16 +1,158 @@
 import unittest
 
-import tests.context
-from wiser.raster import utils
 
 import numpy as np
-from astropy import units as u
+
 from wiser.raster.spectrum import raster_to_combined_rectangles_x_axis, create_raster_from_roi
 from wiser.raster.roi import RegionOfInterest
-from wiser.raster.selection import Selection, RectangleSelection, SinglePixelSelection, PolygonSelection, MultiPixelSelection
+from wiser.raster.selection import RectangleSelection, PolygonSelection, MultiPixelSelection
 from PySide2.QtCore import QPoint
 
+from wiser.gui.app import DataVisualizerApp
+
+from wiser.raster.loader import RasterDataLoader
+from wiser.raster.roi import RegionOfInterest
+
+from wiser.raster.spectrum import ROIAverageSpectrum
+
+import logging
+import traceback
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
 class TestRoiAvgSpectrum(unittest.TestCase):
+
+    # Function to just display the dataset to screen and close
+    def test_roi_avg_spectrum_gui(self):
+        app = QApplication.instance() or QApplication([])  # Initialize the QApplication   
+        wiser_ui = None
+
+        try:
+            # Set up the GUI
+            wiser_ui = DataVisualizerApp()
+            wiser_ui.show()
+
+            loader = RasterDataLoader()
+            N=6
+            np_impl = np.arange(1, N+1).reshape((N, 1, 1)) * np.ones((N, 50, 50))
+            avg_value = np.arange(1, N+1)
+            dataset = loader.dataset_from_numpy_array(np_impl, wiser_ui._data_cache)
+            dataset.set_name("Test_Numpy")
+
+            raster_width = dataset.get_width()
+            raster_height = dataset.get_height()
+
+            roi_one_tenth = RegionOfInterest(name="roi_one_tenth")
+            roi_one_tenth.add_selection(RectangleSelection(QPoint(0, 0), \
+                                                    QPoint(int(raster_width), int(raster_height))))
+            roi_one_tenth.add_selection(PolygonSelection([QPoint(0, 0), \
+                                                            QPoint(int(raster_width/10), int(raster_height/10)), \
+                                                            QPoint(int(raster_width/5), int(raster_height/5))]
+                                                        ))
+            roi_one_tenth.add_selection(MultiPixelSelection([QPoint(0, 0), \
+                                                            QPoint(int(raster_width/2), int(raster_height/2)), \
+                                                            QPoint(int(raster_width/3), int(raster_height/3))]
+                                                        ))
+            
+
+            # Create an application state, no need to pass the app here
+            app_state = wiser_ui._app_state
+
+            # raster_pane = RasterPane(app_state)
+            app_state.add_dataset(dataset)
+            app_state.add_roi(roi_one_tenth)
+
+            main_view = wiser_ui._main_view
+            wiser_ui._main_view._on_show_roi_avg_spectrum(roi_one_tenth, \
+                                                            main_view._rasterviews[(0,0)])
+            spectrum = ROIAverageSpectrum(main_view._rasterviews[(0,0)].get_raster_data(), roi_one_tenth)
+            spectrum._calculate_spectrum()
+            avg_spectrum_arr = spectrum._spectrum
+            
+            np.testing.assert_equal(avg_spectrum_arr, avg_value)
+
+            # This should happen X milliseconds after the above stuff runs
+            QTimer.singleShot(100, app.quit)
+            # Run the application event loop
+            app.exec_()
+
+        except Exception as e:
+            logging.error(f"Application crashed: {e}")
+            traceback.print_exc()
+            self.assertTrue(1==0, f"Falied with error:\n{e}")
+
+        finally:
+            if wiser_ui:
+                wiser_ui.close()
+            app.quit()
+            del app
+
+    def test_roi_avg_spectrum_1_band_gui(self):
+        app = QApplication.instance() or QApplication([])  # Initialize the QApplication   
+        wiser_ui = None
+
+        try:
+            # Set up the GUI
+            wiser_ui = DataVisualizerApp()
+            wiser_ui.show()
+
+            loader = RasterDataLoader()
+            height=50
+            width=50
+            N=1
+            np_impl = np.arange(1, height+1).reshape((1, height, 1)) * np.ones((N, height, width))
+            avg_value = np.mean(np_impl)+10
+            dataset = loader.dataset_from_numpy_array(np_impl, wiser_ui._data_cache)
+            dataset.set_name("Test_Numpy")
+
+            raster_width = dataset.get_width()
+            raster_height = dataset.get_height()
+
+            roi_one_tenth = RegionOfInterest(name="roi_one_tenth")
+            roi_one_tenth.add_selection(RectangleSelection(QPoint(0, 0), \
+                                                    QPoint(int(raster_width), int(raster_height))))
+            roi_one_tenth.add_selection(PolygonSelection([QPoint(0, 0), \
+                                                            QPoint(int(raster_width/10), int(raster_height/10)), \
+                                                            QPoint(int(raster_width/5), int(raster_height/5))]
+                                                        ))
+            roi_one_tenth.add_selection(MultiPixelSelection([QPoint(0, 0), \
+                                                            QPoint(int(raster_width/2), int(raster_height/2)), \
+                                                            QPoint(int(raster_width/3), int(raster_height/3))]
+                                                        ))
+            
+
+            # Create an application state, no need to pass the app here
+            app_state = wiser_ui._app_state
+
+            # raster_pane = RasterPane(app_state)
+            app_state.add_dataset(dataset)
+            app_state.add_roi(roi_one_tenth)
+
+            main_view = wiser_ui._main_view
+            wiser_ui._main_view._on_show_roi_avg_spectrum(roi_one_tenth, \
+                                                            main_view._rasterviews[(0,0)])
+            spectrum = ROIAverageSpectrum(main_view._rasterviews[(0,0)].get_raster_data(), roi_one_tenth)
+            spectrum._calculate_spectrum()
+            avg_spectrum_arr = spectrum._spectrum
+            
+            self.assertNotEqual(avg_spectrum_arr, avg_value)
+
+            # This should happen X milliseconds after the above stuff runs
+            QTimer.singleShot(100, app.quit)
+            # Run the application event loop
+            app.exec_()
+
+        except Exception as e:
+            logging.error(f"Application crashed: {e}")
+            traceback.print_exc()
+            self.assertTrue(1==0, f"Falied with error:\n{e}")
+
+        finally:
+            if wiser_ui:
+                wiser_ui.close()
+            app.quit()
+            del app
 
     def test_raster_to_combined_rectangles1(self):
         raster1 = np.array([
@@ -117,5 +259,3 @@ class TestRoiAvgSpectrum(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         ])
         np.testing.assert_equal(raster, ground_truth)
-
-
