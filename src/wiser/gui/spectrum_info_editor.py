8a73ca0215712d7bf0736c10c5e494c9870f3d30
JoshuaG-K <jgarciakimble@gmail.com>
2025-01-17 11:45:03 -0800

    Took out changes to spectrum_info_editor

--- src/wiser/gui/spectrum_info_editor.py
+++ src/wiser/gui/spectrum_info_editor.py
@@ -8,7 +8,7 @@ from .generated.spectrum_info_editor_ui import Ui_SpectrumInfoEditor
 
 from wiser.raster.spectrum import (SpectrumAverageMode, AVG_MODE_NAMES)
 from wiser.raster.spectrum import (Spectrum, RasterDataSetSpectrum,
-    SpectrumAtPoint, ROIAverageSpectrum, NumPyArraySpectrum)
+    SpectrumAtPoint, ROIAverageSpectrum)
 
 
 class SpectrumInfoEditor(QDialog):
@@ -88,11 +88,11 @@ class SpectrumInfoEditor(QDialog):
             self._ui.lineedit_location.setText(f'Region of Interest:  {roi_name}')
             self._ui.lineedit_location.setEnabled(True)
 
-        elif isinstance(spectrum, NumPyArraySpectrum):
-            self._ui.lineedit_spectrum_type.setText(self.tr('Basic array spectrum'))
+        # elif isinstance(spectrum, LibrarySpectrum):
+        #     self._ui.lineedit_spectrum_type.setText(self.tr('Basic array spectrum'))
         
-            self._ui.lineedit_location.clear()
-            self._ui.lineedit_location.setEnabled(False)
+        #     self._ui.lineedit_location.clear()
+        #     self._ui.lineedit_location.setEnabled(False)
 
             self._ui.lineedit_name.setEnabled(False)
 
