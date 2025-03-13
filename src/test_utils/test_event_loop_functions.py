35925d0aef653bfce9559e845fb50c3ba2443466
JoshuaG-K <jgarciakimble@gmail.com>
2025-03-13 09:17:25 -0700

    Added testing widget to app.py

--- src/test_utils/test_event_loop_functions.py
+++ src/test_utils/test_event_loop_functions.py
@@ -6,17 +6,17 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 class FunctionEvent(QEvent):
-    """
+    '''
     A custom event carrying a single callable (func) that we want
     to run inside the Qt event loop.
-    """
+    '''
     def __init__(self, func):
         # We use the custom event type stored on MyWidget.functionEventType.
         super().__init__(TestingWidget.functionEventType)
         self._func = func
 
     def run(self):
-        """Execute the function carried by this event."""
+        '''Execute the function carried by this event.'''
         self._func()
 
 class TestingWidget(QWidget):
@@ -25,12 +25,20 @@ class TestingWidget(QWidget):
 
     def __init__(self):
         super().__init__()
+        # This attribute ensures the widget does NOT handle mouse events
+        # (so clicks pass through to whatever is beneath).
+        self.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+
+        # Remove any background to make it visually invisible
+        self.setStyleSheet("background: transparent;")
+
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
 
     def event(self, event):
-        """
+        '''
         Override the base event handler to catch our FunctionEvent
         and run its stored callable.
-        """
+        '''
         if event.type() == TestingWidget.functionEventType:
             event.run()   # call the stored function
             return True   # tell Qt we've handled this event
