dd0ebe62a2324103699fa2a1cf6c5ec31938c3c2
JoshuaG-K <jgarciakimble@gmail.com>
2025-05-05 09:00:23 -0700

    Added manual ref end to end test. For now testing
    is done. Fixed bugs with changing the target and
    reference dataset where it would update to the
    wrong previous dataset and with a function
    getting added to the dataset chooser cbox
    signal everytime the geo ref dialog was opened.

--- src/tests/test_geo_ref_gui.py
+++ src/tests/test_geo_ref_gui.py
@@ -30,7 +30,7 @@ class TestGeoReferencerGUI(unittest.TestCase):
         self.test_model.close_app()
         del self.test_model
 
-    def test_general_func(self):
+    def test_rasterpane_e2e(self):
         rel_path = os.path.join("..", "test_utils", "test_datasets", "caltech_4_100_150_nm")
         ds = self.test_model.load_dataset(rel_path)
 
@@ -39,15 +39,6 @@ class TestGeoReferencerGUI(unittest.TestCase):
         self.test_model.set_geo_ref_target_dataset(ds.get_id())
         self.test_model.set_geo_ref_reference_dataset(ds.get_id())
 
-        '''
-        Coords we want to enter:
-        (12, 2), (395615.1733312448, 3778287.4352373052)
-        (22, 133), (395625.94761462574, 3778024.940670322)
-        (142, 131), (395865.7763863942, 3778020.691346924)
-        (144, 4), (395878.48156122735, 3778274.252152171)
-        (80, 84), (395745.30016513495, 3778118.96401564)
-        (44, 99), (395672.2851735266, 3778091.6713232244)
-        '''
         gcp_list = [[(12, 2), (395615.1733312448, 3778287.4352373052)],
                 [(22, 133), (395625.94761462574, 3778024.940670322)],
                 [(142, 131), (395865.7763863942, 3778020.691346924)],
@@ -93,6 +84,73 @@ class TestGeoReferencerGUI(unittest.TestCase):
         warped_transform = ds_warp.get_geo_transform()
         self.assertTrue(np.allclose(warped_transform, ground_truth_geo_transform))
 
+    def test_manual_entry_e2e(self):
+        rel_path = os.path.join("..", "test_utils", "test_datasets", "caltech_4_100_150_nm")
+        ds = self.test_model.load_dataset(rel_path)
+
+        self.test_model.open_geo_referencer()
+
+        self.test_model.set_geo_ref_target_dataset(ds.get_id())
+
+        gcp_list = [[(12, 2), (395615.1733312448, 3778287.4352373052)],
+                [(22, 133), (395625.94761462574, 3778024.940670322)],
+                [(142, 131), (395865.7763863942, 3778020.691346924)],
+                [(144, 4), (395878.48156122735, 3778274.252152171)],
+                [(80, 84), (395745.30016513495, 3778118.96401564)],
+                [(44, 99), (395672.2851735266, 3778091.6713232244)],
+                [(56, 28), (395751.2234671218, 3778276.7707845406)],  # This point is incorrect, we disable it
+                [(64, 122), (395751.2234671218, 3778276.7707845406)]  # This point is incorrect, we remove it
+            ]
+
+        self.test_model.set_interpolation_type('GRA_Bilinear')
+
+        self.test_model.set_output_crs('EPSG', 4326)
+
+        self.test_model.set_polynomial_order("2")
+
+        self.test_model.select_manual_authority("EPSG")
+
+        self.test_model.enter_manual_authority_code("32611")
+
+        self.test_model.click_find_crs()
+
+        self.test_model.choose_manual_crs_geo_ref("EPSG", 4326)
+        self.test_model.choose_manual_crs_geo_ref("EPSG", 32611)
+
+        for gcp in gcp_list:
+            target = gcp[0]
+            ref = gcp[1]
+            lat_north = ref[1]
+            lon_east = ref[0]
+            self.test_model.click_target_image(target)
+            self.test_model.press_enter_target_image()
+            self.test_model.enter_lat_north_geo_ref(lat_north)
+            self.test_model.press_enter_lat_north_geo_ref()
+            self.test_model.enter_lon_east_geo_ref(lon_east)
+            self.test_model.press_enter_lon_east_geo_ref()
+
+        rel_path = os.path.join("..", "test_utils", "test_datasets", "test_warp_output.tif")
+        self.test_model.set_file_save_path(rel_path)
+
+        ground_truth_geo_transform = (-118.13251959615042, 1.9876715549174167e-05, 0.0, 34.14031070448446, 0.0, -1.9876715549174167e-05)
+
+        # Disables and re-enables
+        self.test_model.click_gcp_enable_btn_geo_ref(0)
+        self.test_model.click_gcp_enable_btn_geo_ref(0)
+        # Disables invalid point
+        self.test_model.click_gcp_enable_btn_geo_ref(6)
+
+
+        # Removes invalid point
+        self.test_model.remove_gcp_geo_ref(7)
+
+        self.test_model.click_run_warp()
+
+        ds_warp = self.test_model.load_dataset(rel_path)
+
+        warped_transform = ds_warp.get_geo_transform()
+        self.assertTrue(np.allclose(warped_transform, ground_truth_geo_transform))
+
 
 if __name__ == '__main__':
     '''
@@ -106,7 +164,6 @@ if __name__ == '__main__':
     test_model.open_geo_referencer()
 
     test_model.set_geo_ref_target_dataset(ds.get_id())
-    test_model.set_geo_ref_reference_dataset(ds.get_id())
 
     gcp_list = [[(12, 2), (395615.1733312448, 3778287.4352373052)],
                 [(22, 133), (395625.94761462574, 3778024.940670322)],
@@ -117,19 +174,33 @@ if __name__ == '__main__':
                 [(56, 28), (395751.2234671218, 3778276.7707845406)],  # This point is incorrect, we disable it
                 [(64, 122), (395751.2234671218, 3778276.7707845406)] # This point is incorrect, we remove it 
             ]
-    for gcp in gcp_list:
-        target = gcp[0]
-        ref = gcp[1]
-        test_model.click_target_image(target)
-        test_model.press_enter_target_image()
-        test_model.click_reference_image_spatially(ref)
-        test_model.press_enter_reference_image()
         
     test_model.set_interpolation_type('GRA_Bilinear')
 
     test_model.set_output_crs('EPSG', 4326)
 
     test_model.set_polynomial_order("2")
+
+    test_model.select_manual_authority("EPSG")
+
+    test_model.enter_manual_authority_code("32611")
+
+    test_model.click_find_crs()
+
+    test_model.choose_manual_crs_geo_ref("EPSG", 4326)
+    test_model.choose_manual_crs_geo_ref("EPSG", 32611)
+
+    for gcp in gcp_list:
+        target = gcp[0]
+        ref = gcp[1]
+        lat_north = ref[1]
+        lon_east = ref[0]
+        test_model.click_target_image(target)
+        test_model.press_enter_target_image()
+        test_model.enter_lat_north_geo_ref(lat_north)
+        test_model.press_enter_lat_north_geo_ref()
+        test_model.enter_lon_east_geo_ref(lon_east)
+        test_model.press_enter_lon_east_geo_ref()
 
     rel_path = os.path.join("..", "test_utils", "test_datasets", "test_warp_output.tif")
     test_model.set_file_save_path(rel_path)
@@ -149,6 +220,6 @@ if __name__ == '__main__':
 
     print(f"Warping geo transform: {ds_warp.get_geo_transform()}")
 
-    # test_model.close_geo_referencer()
+    test_model.close_geo_referencer()
 
     test_model.app.exec_()
