28df0045150a04f6101f3554e4d0a4c753fc3f7a
JoshuaG-K <jgarciakimble@gmail.com>
2025-05-27 12:29:14 -0700

    Finished adding similarity transform GUI tests.

--- src/tests/test_similarity_transform_gui.py
+++ src/tests/test_similarity_transform_gui.py
@@ -34,9 +34,9 @@ class TestSimliarityTransformGUI(unittest.TestCase):
     # Get the array and compare. Get this from 
     def test_rotate_scale(self):
         load_path = os.path.join("..", "test_utils", "test_datasets", "caltech_4_100_150_nm")
-        ground_truth_path = os.path.join("..", "test_utils", "test_datasets", "caltech_4_100_150_nm_rot_35_scale_2_linear_gt")
+        ground_truth_path = os.path.join("..", "test_utils", "test_datasets", "caltech_4_100_150_nm_rot_35_scale_2_linear_gt.tif")
 
-        temp_save_path = os.path.join("..", "test_utils", "test_datasets", "generated", "caltech_4_100_150_nm_rot_35_scale_2_linear.tif")
+        temp_save_path = os.path.join("..", "test_utils", "test_datasets", "temp_generated", "caltech_4_100_150_nm_rot_35_scale_2_linear.tif")
         ds = self.test_model.load_dataset(load_path)
 
         self.test_model.open_similarity_transform_dialog()
@@ -67,6 +67,35 @@ class TestSimliarityTransformGUI(unittest.TestCase):
 
         self.assertTrue(np.allclose(gt_arr, test_arr), "Rotated and scaled array doesn't match ground truth")
         self.assertTrue(gt_geo_transform == test_geo_transform, "Rotated and scaled geo transform doesn't match ground truth")
+
+    def test_translate(self):
+        load_path = os.path.join("..", "test_utils", "test_datasets", "caltech_4_100_150_nm")
+        temp_save_path = os.path.join("..", "test_utils", "test_datasets", "temp_generated", "caltech_4_100_150_nm_translate.tif")
+        ds = self.test_model.load_dataset(load_path)
+
+        self.test_model.open_similarity_transform_dialog()
+
+        self.test_model.switch_sim_tab(to_translate=True)
+
+        self.test_model.select_dataset_translate(ds)
+        lat_translate_amt = 100
+        lon_translate_amt = 200
+        self.test_model.set_translate_lat(lat_translate_amt)
+        self.test_model.set_translate_lon(lon_translate_amt)
+
+        self.test_model.set_save_path_translate(temp_save_path)
+
+        self.test_model.run_create_translation()
+
+        self.test_model.close_similarity_transform_dialog()
+
+        ds_translate = self.test_model.load_dataset(temp_save_path)
+
+        orig_gt = ds.get_geo_transform()
+        ground_truth_gt = (orig_gt[0]+lon_translate_amt, orig_gt[1], orig_gt[2], orig_gt[3]+lat_translate_amt, orig_gt[4], orig_gt[5])
+        translated_gt = ds_translate.get_geo_transform()
+        self.assertTrue(ground_truth_gt == translated_gt)
+
         
 
     # Write one test to translate the geo transform by 100, 200. Get the geo transform and comprae
