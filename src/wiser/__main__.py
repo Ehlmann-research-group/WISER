df6af4ebd4c29906e67bcf8c6852c5ce5d3261ab
JoshuaG-K <jgarciakimble@gmail.com>
2026-02-06 16:03:57 -0800

    build: Adding a specific cache dir
    This cache dir shouldn't be deleted when a user's
    local is cleared on a managed system, but it
    might not be able to be made on windows because
    we install into program files there, which needs
    admin perms
    
    Signed-off-by: JoshuaG-K <jgarciakimble@gmail.com>

--- src/wiser/__main__.py
+++ src/wiser/__main__.py
@@ -12,6 +12,17 @@ import multiprocessing
 os.environ["NUMBA_DEBUG_CACHE"] = "1"
 os.environ["NUMBA_DEBUG"] = "1"
 
+if getattr(sys, "frozen", False):
+    # We are in a PyInstaller bundle
+    exe_dir = os.path.dirname(sys.executable)
+else:
+    # Running as a script
+    exe_dir = os.path.dirname(os.path.abspath(__file__))
+numba_cache_dir = os.path.join(exe_dir, "numba_wiser_cache")
+os.makedirs(numba_cache_dir, exist_ok=True)
+
+os.environ["NUMBA_CACHE_DIR"] = numba_cache_dir
+
 # ============================================================================
 # Load gdal plugins into path and set gdal environment variables
 #
