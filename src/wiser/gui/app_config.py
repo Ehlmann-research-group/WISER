ff778be8cb1dc1d5f2da51bc4c5ff37b29a6ee73
JoshuaG-K <jgarciakimble@gmail.com>
2026-03-27 14:11:59 -0700

    refact: Making the WISER data write dir based on version
    Signed-off-by: JoshuaG-K <jgarciakimble@gmail.com>

--- src/wiser/gui/app_config.py
+++ src/wiser/gui/app_config.py
@@ -72,6 +72,23 @@ def resource_path(*parts: str) -> str:
     return str(base.joinpath(*parts))
 
 
+def get_app_data_dir() -> str:
+    """
+    Return the per-version directory WISER should use for writable app data.
+    """
+    sys_name = platform.system()
+    if sys_name == "Windows":
+        return os.path.join(os.path.expandvars(r"%LOCALAPPDATA%\WISER"), version.VERSION)
+
+    if sys_name == "Darwin":
+        return os.path.expanduser(f"~/Library/WISER/{version.VERSION}")
+
+    if sys_name != "Linux":
+        warnings.warn(f'Unrecognized platform name "{sys_name}"')
+
+    return os.path.expanduser(f"~/.wiser/{version.VERSION}")
+
+
 def get_wiser_config_dir() -> str:
     """
     Determine the WISER config directory based on the platform/OS.  The config
@@ -82,24 +99,7 @@ def get_wiser_config_dir() -> str:
     *   All other platforms are assumed to be Linux/*NIX, and `~/.wiser` is
         used.  Note that a warning is output if the platform is not Linux.
     """
-    sys_name = platform.system()
-    if sys_name == "Windows":
-        # Put WISER config in the user's local application data directory
-        wiser_dir = os.path.expandvars(r"%LOCALAPPDATA%\WISER")
-
-    elif sys_name == "Darwin":
-        # Put WISER config in the user's Library directory
-        wiser_dir = os.path.expanduser("~/Library/WISER")
-
-    else:
-        # Use the standard UNIX/Linux approach of a dot-filename in the user's
-        # home directory.
-        if sys_name != "Linux":
-            warnings.warn(f'Unrecognized platform name "{sys_name}"')
-
-        wiser_dir = os.path.expanduser("~/.wiser")
-
-    return wiser_dir
+    return get_app_data_dir()
 
 
 def check_create_wiser_config_dir():
