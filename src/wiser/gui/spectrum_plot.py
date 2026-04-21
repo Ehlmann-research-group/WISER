9820609812c0870312800b475f7d256fe26682fd
JoshuaG-K <jgarciakimble@gmail.com>
2026-04-20 17:30:51 -0700

    feat: Added ability to save single collected spectrum
    Signed-off-by: JoshuaG-K <jgarciakimble@gmail.com>

--- src/wiser/gui/spectrum_plot.py
+++ src/wiser/gui/spectrum_plot.py
@@ -1226,6 +1226,9 @@ class SpectrumPlotGeneric(QWidget):
                 act = menu.addAction(self.tr("Edit..."))
                 act.triggered.connect(lambda *args, treeitem=treeitem: self._on_edit_spectrum(treeitem))
 
+            act = menu.addAction(self.tr("Save to file..."))
+            act.triggered.connect(lambda *args, treeitem=treeitem: self._on_save_single_spectrum(treeitem))
+
             if spectrum.is_discardable():
                 menu.addSeparator()
 
@@ -1345,6 +1348,23 @@ class SpectrumPlotGeneric(QWidget):
                 spectra.append(treeitem.data(0, Qt.UserRole))
 
             export_spectrum_list(selected[0], spectra)
+
+    def _on_save_single_spectrum(self, treeitem):
+        spectrum = treeitem.data(0, Qt.UserRole)
+        supported_formats = [
+            self.tr("Text files (*.txt)"),
+            self.tr("All files (*)"),
+        ]
+
+        selected = QFileDialog.getSaveFileName(
+            self,
+            self.tr("Save Spectrum"),
+            self._app_state.get_current_dir(),
+            ";;".join(supported_formats),
+        )
+
+        if len(selected[0]) > 0:
+            export_spectrum_list(selected[0], [spectrum])
 
     def _on_discard_spectrum(self, treeitem, display_confirm=True):
         spectrum = treeitem.data(0, Qt.UserRole)
@@ -1985,6 +2005,9 @@ class SpectrumPlot(SpectrumPlotGeneric):
             if spectrum.is_editable():
                 act = menu.addAction(self.tr("Edit..."))
                 act.triggered.connect(lambda *args, treeitem=treeitem: self._on_edit_spectrum(treeitem))
+
+            act = menu.addAction(self.tr("Save to file..."))
+            act.triggered.connect(lambda *args, treeitem=treeitem: self._on_save_single_spectrum(treeitem))
 
             # Add plugin menu items
             add_plugin_context_menu_items(
